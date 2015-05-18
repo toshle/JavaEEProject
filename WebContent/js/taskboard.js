b75ec3c00e64e1dda4f29222e684325791be7656
toshle <rosen_ra4ev@abv.bg>
2015-05-18 18:38:34 +0300

    Add initial taskboard mockup UI

---
+++ WebContent/js/taskboard.js
@@ -0,0 +1,54 @@
+$('document').ready(init);
+
+function init(){
+  $("#todo, #inprogress, #done").sortable({
+
+    items: "li:not(.section-title)",
+
+    connectWith: ".connectedSortable",
+
+    receive: function( event, ui ) {
+      console.log(event.target.getAttribute("id"));
+      console.log(event.originalEvent.target.dataset["id"]);
+    }
+
+  }).disableSelection();
+
+  $(".task-title").bind("click", function(event) {
+    console.log(event.originalEvent);
+    alert("Task " + event.originalEvent.target.dataset["id"]);
+
+    event.preventDefault();
+  });
+
+  $("#add-task-link").bind("click", function(event) {
+    alert("Add new task " + event);
+
+    event.preventDefault();
+  });
+
+  $(".username-link").bind("click", function(event) {
+    console.log(event.originalEvent);
+    alert("Username " + event.originalEvent.target.text);
+
+    event.preventDefault();
+  });
+
+  $("#logout-link").bind("click", function(event) {
+    alert("Logout ");
+
+    event.preventDefault();
+  });
+
+  $("#login-link").bind("click", function(event) {
+    alert("Login ");
+
+    event.preventDefault();
+  });
+
+  $("#register-link").bind("click", function(event) {
+    alert("Register ");
+    
+    event.preventDefault();
+  });
+}
